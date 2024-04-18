788d948a452ca095e09d18cd0addffbc16bb5baa
Yugal Khanal <yugalkhanal5@gmail.com>
2024-04-18 04:41:33 +0100

    noice

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 vim.cmd("set expandtab")
+vim.cmd("set number")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard+=unnamedplus")
+-- vim.cmd("set foldmethod=manual")
+vim.cmd("set foldcolumn=1")
+vim.cmd("set foldlevel=20")
+vim.cmd("set foldmethod=expr")
+vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
