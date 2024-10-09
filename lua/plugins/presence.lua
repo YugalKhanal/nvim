5c4edbc08cd6fd6651118457891cd60bb615568d
Yugal Khanal <yugalkhanal5@gmail.com>
2024-10-09 10:15:01 +0100

    added vim-tmux-nav

--- lua/plugins/presence.lua
+++ lua/plugins/presence.lua
@@ -9,8 +9,8 @@ return {
       neovim_image_text   = "Did you mean: emacs", -- Text displayed when hovered over the Neovim image
       main_image          = "neovim",                 -- Main image display (either "neovim" or "file")
       log_level           = nil,                      -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-      debounce_timeout    = 2,                       -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-      enable_line_number  = false,                    -- Displays the current line number instead of the current project
+      debounce_timeout    = 1,                       -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+      enable_line_number  = true,                    -- Displays the current line number instead of the current project
       blacklist           = {},                       -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
       buttons             = true,                     -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
       file_assets         = {},                       -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
