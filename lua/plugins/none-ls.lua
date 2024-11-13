1a510c2144099c35a55972a5f2b380b0ddc64e10
yugalkhanal <yugalkhanal5@gmail.com>
2024-11-13 23:03:18 +0000

    added lazygit

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,3 @@
--- return {
---   "nvimtools/none-ls.nvim",
---   config = function()
---     local null_ls = require("null-ls")
---     null_ls.setup({
---       sources = {
---         null_ls.builtins.formatting.stylua,
---         null_ls.builtins.formatting.prettier,
---         null_ls.builtins.formatting.nixpkgs_fmt,
---         null_ls.builtins.formatting.clang_format,
---         null_ls.builtins.formatting.latexindent,
---         -- null_ls.builtins.diagnostics.black,
---       },
---     })
---     vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { desc = "Format the code with LSP" })
---   end,
--- }
-
-
-
 return {
   "nvimtools/none-ls.nvim",
   config = function()
